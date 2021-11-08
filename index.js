d6bc554c2df98fd85375fa057f0d159c773ea304
Doris Morales <moralesdoris880@gmail.com>
2021-11-08 10:45:25 -0500

    commit tree 09a55804d591e43e685951a6689330a1babbddba

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    console.log("test")
+    document.getElementById("text").textContent = "This is really cool!"
+
+})
